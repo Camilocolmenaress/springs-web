2858772255595661f4a4254661e141ac3b5ca467
camilocolmenares2 <camilo.colmenares2005@gmail.com>
2026-05-21 20:14:22 -0500

    feat(CAM): simplify MobileCanvas to vertical scroll

--- src/components/MobileCanvas.tsx
+++ src/components/MobileCanvas.tsx
@@ -1,46 +1,24 @@
 "use client";
 
-import { useRef } from "react";
 import MobileEditorial from "@/components/MobileEditorial";
-import MobilePedir from "@/components/MobilePedir";
 
 export default function MobileCanvas() {
-  const wrapperRef = useRef<HTMLDivElement>(null);
-
-  const scrollToStart = () => {
-    wrapperRef.current?.scrollTo({ left: 0, behavior: "smooth" });
-  };
-
   return (
     <div
-      ref={wrapperRef}
-      className="mobile-canvas"
       style={{
         position: "fixed",
         top: 0,
         left: 0,
         width: "100%",
         height: "100%",
-        overflowX: "scroll",
-        overflowY: "hidden",
+        overflowY: "auto",
+        overflowX: "hidden",
         scrollbarWidth: "none",
-        touchAction: "pan-x",
-        background: "var(--cream)",
-        scrollSnapType: "x mandatory",
         WebkitOverflowScrolling: "touch",
+        background: "#F2E8D5",
       }}
     >
-      <div
-        style={{
-          display: "flex",
-          height: "100vh",
-          width: "400vw",
-          alignItems: "stretch",
-        }}
-      >
-        <MobileEditorial />
-        <MobilePedir onBack={scrollToStart} />
-      </div>
+      <MobileEditorial />
     </div>
   );
 }
